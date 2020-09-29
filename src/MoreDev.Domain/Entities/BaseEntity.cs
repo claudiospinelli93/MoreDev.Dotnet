879d45260487f905cdbc0494f336e7c14649baa9
Claudio Spinelli <claudiomanoel93@gmail.com>
2020-09-29 11:59:24 -0300

    ✅ Add entity files.

---
+++ src/MoreDev.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoreDev.Domain.Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime InsertDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
---
+++ src/MoreDev.Domain/Entities/HelloMoreDev.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoreDev.Domain.Entities
+{
+    public class HelloMoreDev : BaseEntity
+    {
+        [Required]
+        [MaxLength(150)]
+        public string Nome { get; set; }
+
+        [Required]
+        public int Idade { get; set; }
+    }
+}
