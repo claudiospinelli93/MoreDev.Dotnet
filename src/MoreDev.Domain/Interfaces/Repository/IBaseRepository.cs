3300cad7959ff8543f24e79cde062b95f6b41ebc
Claudio Spinelli <claudiomanoel93@gmail.com>
2020-09-30 11:09:07 -0300

    ✅ Add interface repository.

---
+++ src/MoreDev.Domain/Interfaces/Repository/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MoreDev.Domain.Interfaces.Repository
+{
+    public interface IBaseRepository<TEntity> : IDisposable where TEntity : class
+    {
+        Task<TEntity> AddAsync(TEntity entity, CancellationToken ct);
+        Task<TEntity> GetByIdAsync(object id, CancellationToken ct);
+        Task<int> RemoveAsync(TEntity entity, CancellationToken ct);
+        Task<bool> RemoveAsync(object id, CancellationToken ct);
+    }
+}
