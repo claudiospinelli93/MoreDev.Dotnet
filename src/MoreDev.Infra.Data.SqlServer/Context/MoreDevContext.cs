e6a48b55b614fe91aa5a250e1544aa93513a3d81
Claudio Spinelli <claudiomanoel93@gmail.com>
2020-09-30 14:35:12 -0300

    ✅ Add context in project.

---
+++ src/MoreDev.Infra.Data.SqlServer/Context/MoreDevContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoreDev.Domain.Entities;
+
+namespace MoreDev.Infra.Data.SqlServer.Context
+{
+    public class MoreDevContext : DbContext
+    {
+        public DbSet<HelloMoreDevEntity> HelloMoreDev { get; set; }
+    }
+}
